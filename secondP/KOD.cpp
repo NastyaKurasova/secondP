9e7acbc850cfd80c71ec50d81b6a638d7749fdd8
”Andrey <fylhtq180419782285@mail.ru>
2022-07-26 19:55:55 +1000

    Добавил ifstream.

--- secondP/KOD.cpp
+++ secondP/KOD.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-
+#include <vector>
 using namespace std;
 
 int main()
@@ -8,4 +8,7 @@ int main()
 	setlocale(LC_CTYPE, "rus");
 	cout << "Hallo!" << "\n";
 	cout << "Что теперь?";
+	ifstream fin{ "DZ.txt" };
+	vector <double> a;
+	//for (double x; fin >> x;) a.push_back(x);
 }
